dc659c3963b398014fce5e8f06da96bcb4ed00e5
Changyun Yoo <Changyun Yoo>
2017-02-12 21:07:55 -0800

    added index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>main</title>
+	<style>
+	.img_div {
+		text-align: justify;
+		width: 750px;
+		}
+	.img_div img{
+		display: block;
+		margin: 0 auto;
+	}
+  </style>
+  </head>
+  <body>
+  	<header> 
+	<center>
+	<?php include 'main_menu.php';?>
+	</center>
+	</header>
+	<center>
+    <h1>Welcome!</h1>
+	
+	<!-- upload img -->
+	<div class="img_div">
+	<img src="pic/main_light_waves.jpg" alt="main_light_waves" height="500" width="750">
+	<center><figcaption>Light waves on 101</figcaption></center>
+	
+
+
+
+	<p align="left">
+	<b>09/09/16 F:&nbsp;</b>Massive photo updates!<br>
+	<b>07/24/16 S:&nbsp;</b>Photos of Oceano Dunes updated.<br>
+	<b>07/11/16 M:&nbsp;</b>Random pics uploaded.(check out the cool slideshow.)<br>
+	<b>07/02/16 S:&nbsp;</b>Yosemite pics uploaded.<br>
+	<b>06/29/16 W:&nbsp;</b>Posts menu updated (thanks to JNK).<br>
+	<b>06/11/16 S:&nbsp;</b>Guestbook feature added. Now you can leave comments!<br>
+	<b>05/24/16 T:&nbsp;</b>Pictures of Amherst uploaded.<br>
+	<b>05/14/16 S:&nbsp;</b>Check out new posts on SB.<br>
+	<b>05/08/16 S:&nbsp;</b>SF pictures uploaded.<br>
+	<b>04/27/16 W:&nbsp;</b>I finally reopened my site. There will be more updates soon, so stay tuned.
+	
+	
+	</p>
+	</div>
+	</center>
+  </body>
+</html>
